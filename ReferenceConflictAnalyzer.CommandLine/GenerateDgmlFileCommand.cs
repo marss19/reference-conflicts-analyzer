f503363bec35d1126e01c3388ca5275ec9e7d033
marss19 <marss.ua@gmail.com>
2018-05-19 21:48:30 +0200

    added command line utility

---
+++ ReferenceConflictAnalyzer.CommandLine/GenerateDgmlFileCommand.cs
@@ -0,0 +1,78 @@
+﻿using ReferenceConflictAnalyser;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ReferenceConflictAnalyzer.CommandLine
+{
+    class GenerateDgmlFileCommand
+    {
+        public void Run(string[] args)
+        {
+            ValidateParameters(args);
+
+            var dgml = Workflow.CreateDependenciesGraph(_filePath, _configPath, _ignoreSystemAssemblies);
+            var outputFileName = Path.Combine(_outputFolder, $"{DateTime.Now.ToString("dd-MM-yyyy hh-MM-ss")}.dgml");
+            File.WriteAllText(outputFileName, dgml);
+        }
+
+        #region private 
+
+        private const string ParameterPattern = "^-(?<name>\\w+)=(?<value>.*)$";
+        private string _filePath;
+        private string _configPath;
+        private bool _ignoreSystemAssemblies = true;
+        private string _outputFolder;
+
+
+        private void ValidateParameters(string[] args)
+        {
+            var re = new Regex(ParameterPattern, RegexOptions.IgnoreCase);
+
+            foreach (var arg in args)
+            {
+                var match = re.Match(arg);
+                if (match.Success)
+                {
+                    var value = match.Groups["value"].Value;
+                    switch (match.Groups["name"].Value)
+                    {
+                        case "file":
+                            _filePath = value;
+                            break;
+
+                        case "config":
+                            _configPath = value;
+                            break;
+
+                        case "ignoreSystemAssemblies":
+                            bool t;
+                            if (bool.TryParse(value, out t))
+                                _ignoreSystemAssemblies = t;
+                            else
+                                throw new ArgumentException("Incorrect value of 'ignoreSystemAssemblies' parameter");
+                            break;
+
+                        case "output":
+                            _outputFolder = value;
+                            break;
+                    }
+
+                }
+            }
+
+            if (string.IsNullOrEmpty(_filePath))
+                throw new Exception("'file' parameter is mandatory.");
+
+            if (string.IsNullOrEmpty(_outputFolder))
+                throw new Exception("'output' parameter is mandatory.");
+
+        }
+
+        #endregion
+    }
+}
---
+++ ReferenceConflictAnalyzer.CommandLine/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ReferenceConflictAnalyzer.CommandLine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                if (args == null
+                    || !args.Any()
+                    || args[0] == "?"
+                    || args[0] == "help")
+                {
+                    Console.WriteLine(@"Parameters: 
+
+ -file - (mandatory) path to assembly (DLL or executable) to analyze;
+ -config - (optional) path to related config file;
+ -ignoreSystemAssemblies - (optional, default value is 'true') exclude from the analysis assemblies with names starting with 'System';
+ -output - (mandatory) directory to put genegated DGML file to.");
+                    Console.ReadKey();
+                }
+                else
+                    new GenerateDgmlFileCommand().Run(args);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(ex.StackTrace);
+                Console.ReadKey();
+            }
+        }
+
+        
+    }
+}
